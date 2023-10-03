517a7a106582b4d5e18f5d3d7b12dda422e86699
julianmuheirweps <jmu@uniscale.com>
2023-10-03 17:22:58 +0300

    init commit

---
+++ src/main/java/com/modelingtest/AppService.java
@@ -0,0 +1,8 @@
+package com.modelingtest;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppService {
+
+}
---
+++ src/main/java/com/modelingtest/ModelingTestApplication.java
@@ -0,0 +1,13 @@
+package com.modelingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelingTestApplication.class, args);
+    }
+
+}
